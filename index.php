8f7f36c74dd4f08e37706c683d07561a16be1cdc
radislavsplichal <radislavsplichal@gmail.com>
2015-03-07 11:38:03 +0100

    Start

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    echo("We are awesome!")
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+    <head>
+        <meta charset="utf-8" />
+        <title> Phonetics & Phonology</title>
+    </head>
+    <body>
+        <header>
+            <img src="header.jpg" alt="Phonetics & Phonology">
+        
+        </header>
+        <nav>
+            <a></a>
+            <a></a>
+            <a></a>
+        </nav>
+        <section>
+        
+            <article>
+                <h1>Welcome</h1>
+                <p> Phonetics</p>
+                <iframe width="1280" height="720" src="https://www.youtube.com/embed/56mKJmdjuqg" ></iframe>
+            </article>
+        
+        
+        </section>
+        <footer>
+            <p>Property of Technical University of Liberec / MIT licence / Radislav Šplíchal and Jakub Krejčí</p>
+        
+        </footer>
+    </body>
+</html>
