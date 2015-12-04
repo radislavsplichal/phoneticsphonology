6eb35ae4ed0abe9e21ac3ea8910fb73860b6d511
sonahlavackova <sonahlavackova@10.0.0.61>
2015-12-04 21:46:27 +0100

    fixture2

--- server/posts.js
+++ server/posts.js
@@ -6,3 +6,13 @@
 
 Meteor.publish('posts', function() { return Posts.find();
 });
+
+if (Posts.find() === 0) {
+    Posts.insert({
+        title: 'My Awesome Posts',
+        author: 'Radislav Ruda',
+        content: 'Another interesting post',
+        mediacontent: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/xc6cfJztR8A\' frameborder=\'0\' allowfullscreen></iframe>'
+
+    });
+}
