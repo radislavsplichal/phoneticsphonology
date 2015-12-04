b5568d52cca91a4f5f251ea5790198fab58d3376
sonahlavackova <sonahlavackova@10.0.0.61>
2015-12-04 21:46:50 +0100

    fixture2

--- client/postData.js
+++ client/postData.js
@@ -6,12 +6,3 @@ Template.postsList.helpers({
     }
 });
 
-if (Posts.find() === 0) {
-    Posts.insert({
-        title: 'My Awesome Posts',
-        author: 'Radislav Ruda',
-        content: 'Another interesting post',
-        mediacontent: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/xc6cfJztR8A\' frameborder=\'0\' allowfullscreen></iframe>'
-
-    });
-}
