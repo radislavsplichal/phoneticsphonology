b59205bc9ca60e0057e90a59bb1e9be0d6028d51
sonahlavackova <sonahlavackova@10.0.0.61>
2015-12-05 10:29:59 +0100

    User Accounts

---
+++ client/signinExtras.js
@@ -0,0 +1,36 @@
+Accounts.ui.config({
+    requestPermissions: {},
+    extraSignupFields: [{
+        fieldName: 'first-name',
+        fieldLabel: 'First name',
+        inputType: 'text',
+        visible: true,
+        validate: function(value, errorFunction) {
+          if (!value) {
+            errorFunction("Please write your first name");
+            return false;
+          } else {
+            return true;
+          }
+        }
+    }, {
+        fieldName: 'last-name',
+        fieldLabel: 'Last name',
+        inputType: 'text',
+        visible: true,
+    },  {
+        fieldName: 'terms',
+        fieldLabel: 'I accept the terms and conditions',
+        inputType: 'checkbox',
+        visible: true,
+        saveToProfile: false,
+        validate: function(value, errorFunction) {
+            if (value) {
+                return true;
+            } else {
+                errorFunction('You must accept the terms and conditions.');
+                return false;
+            }
+        }
+    }]
+});
