0c02210545f63d0272128d28a6e60890c8cb51b2
sonahlavackova <sonahlavackova@192.168.0.100>
2015-12-08 16:10:13 +0100

    changes

--- client/router.js
+++ client/router.js
@@ -7,15 +7,28 @@ Router.route('/', function() {
 Router.route('/newArticle', function() {
     this.render('newArticle');
 });
-Router.route('/exercises', function(){
-  this.render('exercises', {
-    data: { 'exercises' : function () {
-      return Exercises.find();
+Router.route('/exercisesByCategory', function(){
+  this.render('exercisesCategories', {
+    data: { 'categories' : function () {
+      return _.uniq(Exercises.find().map(function(x){return x.type}));
     }
   }
   });
 });
-Router.route("/exercises/:_id", function(){
+
+Router.route("/exercises/:type", function(){
+  this.render("exercisesSorted", {
+    data: function(){
+      console.log(this.params);
+      // console.log(Exercises.findOne({_id: this.params._id}));
+      return Exercises.find({type: this.params.type});
+    }
+  });
+
+},{name: 'exercises'});
+
+
+Router.route("/exercisesID/:_id", function(){
   this.render("exerciseDetail", {
     data: function(){
       // console.log(Exercises.findOne({_id: this.params._id}));
--- server/exercises.js
+++ server/exercises.js
@@ -19,4 +19,16 @@ if(Exercises.find().count() === 0) {
     instructions: 'Read the list of words. Which of the underlined sounds is different? Click on the word.',
     options: ['cat', 'fan', 'many', 'black', 'pack', 'chap', 'dad', 'happy', 'back', 'sad']
   })
+  Exercises.insert({
+    type: 'matching',
+    title: 'Matching 1',
+    instructions:'Look at the words below. Type them in the table according to the vowel sound in the word.',
+    options: ['man', 'stand', 'rest', 'rang', 'men', 'red', 'lap', 'left', 'ten', 'sat', 'bed', 'bang']
+  })
+  Exercises.insert({
+    type: 'matching',
+    title: 'Matching 2',
+    instructions:'Look at the words below. Type them in the table according to the vowel sound in the word.',
+    options: ['man', 'stand', 'breast', 'suck', 'luck', 'lap', 'left', 'ten', 'sat', 'bed', 'bang']
+  })
 }
