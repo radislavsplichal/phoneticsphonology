022a8459f1254d7b189b665032b559a89cb6d3bc
sonahlavackova <sonahlavackova@10.0.0.61>
2016-02-13 12:45:24 +0100

    Matching Exercises Working

--- server/exercises.js
+++ server/exercises.js
@@ -1,18 +1,21 @@
 Meteor.publish('exercises', function() { return Exercises.find();
 });
-if(Exercises.find().count() === 5) {
+if(Exercises.find().count() === 3) {
 
   Exercises.insert({
     type: 'matching',
     title: 'Matching x',
     instructions:'Look at the words below. Type them in the table according to the vowel sound in the word.',
     options: ['man', 'stand', 'rest', 'rang', 'men', 'red', 'lap', 'left', 'ten', 'sat', 'bed', 'bang'],
-    value: ['/æ/','/e/']
+    value: ['/æ/','/e/'],
+    solution: [["/æ/","man"],["/æ/","stand"],["/e/","rest"],["/æ/","rang"],["/e/","men"],["/e/","red"],["/æ/","lap"],["/e/","left"],["/e/","ten"],["/æ/","sat"],["/e/","bed"],["/æ/","bang"]]
+
   })
   Exercises.insert({
     type: 'matching',
     title: 'Matching 2',
     instructions:'Look at the words below. Type them in the table according to the vowel sound in the word.',
-    options: ['man', 'stand', 'breast', 'suck', 'luck', 'lap', 'left', 'ten', 'sat', 'bed', 'bang']
+    options: ['man', 'stand', 'breast', 'suck', 'luck', 'lap', 'left', 'ten', 'sat', 'bed', 'bang'],
+    value: ['/æ/','/e/']
   })
 }
