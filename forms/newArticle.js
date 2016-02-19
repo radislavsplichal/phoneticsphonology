2d0b67f83ba6c0a15b22e7ec184ea0a055cfddf5
radislavsplichal <radislavsplichal@gmail.com>
2016-02-19 16:51:01 +0100

    Administration Added
    Administration section added. New Article Form submission and from
    added.
    
    Form Status: always true. To be fixed.

---
+++ forms/newArticle.js
@@ -0,0 +1,46 @@
+if(Meteor.isClient){
+// State of the form before submission
+  Template.newArticle.created = function (){
+
+    this.state = new ReactiveDict();
+    this.state.set('message');
+    console.log(this.state);
+  }
+
+// newArticle form handling after submission
+  Template.newArticle.events({
+    "submit .newArticle": function(event){
+
+      event.preventDefault();
+      //Extracting the data from the form
+      var title = event.target.heading.value;
+      //console.log(title);
+      var content = event.target.text.value;
+      var author = "Administrator"
+
+      //Insert the Article into the collection
+      Posts.insert({
+
+        createdAt: new Date(), // current time
+        title: title ,
+        author: author ,
+        content: content
+
+
+      })
+
+      console.log("Article Inserted");
+
+    }
+  });
+
+
+  Template.newArticle.helpers({
+    state : function(){
+     console.log(this.state);
+     return Template.instance().state;
+
+   }
+
+  });
+}
--- server/posts.js
+++ server/posts.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,9 +9,9 @@ Meteor.publish('posts', function() { return Posts.find();
 
 if (Posts.find().count() === 0) {
     Posts.insert({
-        title: 'My Awesome Posts',
+        title: 'Common Mispronunciation errors by John Green',
         author: 'Radislav Ruda',
-        content: 'Another interesting post',
+        content: 'I found this very exciting video explaining very common mistakes people make in every day language. Mental Floss is a great source of short and comprehensible videos for anyone.  ',
         mediacontent: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/xc6cfJztR8A\' frameborder=\'0\' allowfullscreen></iframe>'
 
     });
