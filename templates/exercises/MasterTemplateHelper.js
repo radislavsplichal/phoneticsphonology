ef7a37b3ee7cd2b3d830ea22d050765c4529cf3b
radislavsplichal <radislavsplichal@Sona-MacBook-Pro.local>
2016-04-20 12:10:26 +0200

    Verification fixed again
    Update to veryfy method needed

--- templates/exercises/MasterTemplateHelper.js
+++ templates/exercises/MasterTemplateHelper.js
@@ -1,60 +1,121 @@
+
+
+
 if(Meteor.isClient){
-  Template.MasterTemplate.created = function (){
 
-    this.state = new ReactiveDict();
-    this.state.set('message', "N/A");
-    console.log(this.state);
-  }
+  // ReactiveDict - status of completion defined here
+  Template.MasterTemplate.onCreated( function (){
 
-Template.MasterTemplate.helpers({
-   state : function(){
-    console.log(this.state + "helpers");
-    return Template.instance().state;
+    this.showResult = new ReactiveDict();
+    this.showResult.set('message', "N/A");
+    //console.log(this.showResult);
+  });
+  // Get the state of ReactiveDict
+  Template.MasterTemplate.helpers({
+    state : function(){
+      //console.log(Template.instance().showResult.get('message'));
+      return Template.instance().showResult.get('message');
 
-  }
-
-  //  allData: function(){
-  //   var exercises =
-  //
-  // {
-  //     type: 'odd-one-out',
-  //     title: "Odd one out",
-  //     instructions: 'Read the list of words. Which of the underlined sounds is different? Click on the word.',
-  //     options: ['cat', 'fan', 'many', 'attack', 'pack', 'sang', 'dad', 'happy', 'back', 'sad'],
-  //     solution: ['attack']
-  // };
-  //   //console.log(exercises);
-  //    return exercises;
-  //  }
+    }
 
 
+  });
+  //Submission Event handling and mapping answers
+  Template.MasterTemplate.events({
+    'submit form': function(event, template){
+      event.preventDefault();
+      var exercisesID = event.target.exercisesID.value
+      var selected = template.findAll( "input[type=checkbox]:checked");
+      var array = _.map(selected, function(item) {
+        return item.defaultValue;
+      });
 
-});
-Template.MasterTemplate.events({
-  'submit form': function(event, template){
-    event.preventDefault();
-    var exercisesID = event.target.exercisesID.value
-    var selected = template.findAll( "input[type=checkbox]:checked");
-    var array = _.map(selected, function(item) {
-       return item.defaultValue;
-     });
+      //console.log(array);
+      //console.log(exercisesID);
 
-     console.log(array);
-     console.log(exercisesID);
-     Meteor.call('verifyExercise', array, exercisesID, function (error, result){
-       if (result) {
-       console.log("Correct!");
-       template.state.set('message', "Good Job!");
-     } else {
-       console.log("You got it Wrong!");
-       template.state.set('message', "Bad Job!");
-     }
-     console.log(error);
-   })
-}
-});
+    }
+  });
 
 
 
+
+
+
+
+
+  Template.MasterTemplate.events({
+
+    'submit form': function(event, template){
+      event.preventDefault();
+      var exercisesID = event.target.exercisesID.value;
+      var type = event.target.type.value;
+
+      function test (){
+        Meteor.call('verifyExercise', array, exercisesID, function (error, result){
+          if (result) {
+            console.log("Correct!");
+            template.showResult.set('message', {
+              text : "Good Job!",
+              type : "alert-success"
+            });
+            //console.log(Template.instance().showResult.get('message'));
+          } else {
+            console.log("You got it Wrong!");
+            template.showResult.set('message', {
+              text: "Not quite right. Try another time!",
+              type: "alert-danger"
+            });
+            //console.log(Template.instance().showResult.get('message'));
+
+          }
+          console.log(error);
+        });
+      }
+
+
+
+
+
+      // Exercise Correction Mechanism
+      switch (type) {
+        case "matching":
+        console.log(type);
+
+        var selected = template.findAll( "input[type=radio]:checked");
+        var array = _.map(selected, function(item){
+          return [item.value, item.name]
+        });
+        test();
+        break;
+
+        case "Listening-matching":
+        console.log(type);
+
+        var selected = template.findAll( "input[type=radio]:checked");
+        var array = _.map(selected, function(item){
+          return [item.value, item.name]
+        });
+        test();
+
+        break;
+
+
+        case "odd-one-out":
+
+        var selected = template.findAll( "input[type=radio]:checked");
+        var array = _.map(selected, function(item) {
+          return item.defaultValue;
+        });
+
+        console.log(array);
+        console.log(exercisesID);
+        test();
+
+
+
+        break;
+      }
+    }}
+  );
 
 }
